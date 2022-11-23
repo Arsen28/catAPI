2a200175aabe665634c027aa73cc30a12c67aed3
Arsen <arsen281296@gmail.com>
2022-11-23 21:40:58 +0400

    fix loader

--- src/App.tsx
+++ src/App.tsx
@@ -7,28 +7,22 @@ const Cats = lazy(() => import("./features/cats/Cats"));
 
 const Loader = () => {
   return (
-    <div className="loader">
-      <img src="https://loading.io/spinners/flask/index.flask-loader.svg"/>
       <span>Working on it...</span>
-    </div>
   );
 }
 
-
 function App() {
   return (
-    <Suspense fallback={<Loader/>}>
     <Router>
       <div className="App">
         <Layout>
           <Routes>
-          <Route  path="/" element={<Suspense fallback={<Loader/>}></Suspense>} />
+          <Route  path="/" element={<Suspense fallback={<Loader/>}><Cats /></Suspense>} />
           <Route  path="/:categoryId" element={<Suspense fallback={<Loader/>}><Cats /></Suspense>} />
           </Routes>
         </Layout>
       </div>
     </Router>
-    </Suspense>
   );
 }
 
